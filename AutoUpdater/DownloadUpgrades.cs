7e6da2e67b4d57d858125ad20cf1481a23d2a6f3
Richard Wooters <Richard@rbj.com>
2022-11-03 14:43:52 -0700

    Added DownloadUpgrades

---
+++ AutoUpdater/DownloadUpgrades.cs
@@ -0,0 +1,281 @@
+﻿using KnightsWarriorAutoupdater;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoUpdater
+{
+
+    public class DownloadUpgrades
+    {
+        #region The private fields
+        private bool isFinished = false;
+        private List<DownloadFileInfo> downloadFileList = null;
+        private List<DownloadFileInfo> allFileList = null;
+        private ManualResetEvent evtDownload = null;
+        private ManualResetEvent evtPerDonwload = null;
+        private WebClient clientDownload = null;
+
+        long total = 0;
+        long nDownloadedTotal = 0;
+
+        #endregion
+
+        public DownloadUpgrades(List<DownloadFileInfo> downloadFileList)
+        {
+            this.downloadFileList = downloadFileList ?? throw new ArgumentNullException(nameof(downloadFileList));
+            allFileList = new List<DownloadFileInfo>();
+            foreach (DownloadFileInfo file in downloadFileList)
+            {
+                allFileList.Add(file);
+            }
+
+        }
+
+        public bool Start()
+        {
+            evtDownload = new ManualResetEvent(true);
+            evtDownload.Reset();
+            ThreadPool.QueueUserWorkItem(new WaitCallback(this.ProcDownload));
+            evtDownload.WaitOne();
+            return true;    
+        }
+
+
+        private void ProcDownload(object o)
+        {
+            string tempFolderPath = Path.Combine(CommonUnitity.SystemBinUrl, ConstFile.TEMPFOLDERNAME);
+            if (!Directory.Exists(tempFolderPath))
+            {
+                Directory.CreateDirectory(tempFolderPath);
+            }
+
+
+            evtPerDonwload = new ManualResetEvent(false);
+
+            foreach (DownloadFileInfo file in this.downloadFileList)
+            {
+                total += file.Size;
+            }
+            try
+            {
+                while (!evtDownload.WaitOne(0, false))
+                {
+                    if (this.downloadFileList.Count == 0)
+                        break;
+
+                    DownloadFileInfo file = this.downloadFileList[0];
+
+
+                    //Debug.WriteLine(String.Format("Start Download:{0}", file.FileName));
+
+
+                    //Download
+                    clientDownload = new WebClient();
+
+                    //Added the function to support proxy
+                    //clientDownload.Proxy = System.Net.WebProxy.GetDefaultProxy();
+                    clientDownload.Proxy.Credentials = CredentialCache.DefaultCredentials;
+                    clientDownload.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                    //End added
+
+                    //clientDownload.DownloadProgressChanged += (object sender, DownloadProgressChangedEventArgs e) =>
+                    //{
+                    //    try
+                    //    {
+                    //        this.SetProcessBar(e.ProgressPercentage, (int)((nDownloadedTotal + e.BytesReceived) * 100 / total));
+                    //    }
+                    //    catch
+                    //    {
+                    //        //log the error message,you can use the application's log code
+                    //    }
+
+                    //};
+
+                    clientDownload.DownloadFileCompleted += (object sender, AsyncCompletedEventArgs e) =>
+                    {
+                        try
+                        {
+                            DealWithDownloadErrors();
+                            DownloadFileInfo dfile = e.UserState as DownloadFileInfo;
+                            nDownloadedTotal += dfile.Size;
+                            //this.SetProcessBar(0, (int)(nDownloadedTotal * 100 / total));
+                            evtPerDonwload.Set();
+                        }
+                        catch (Exception)
+                        {
+                            //log the error message,you can use the application's log code
+                        }
+
+                    };
+
+                    evtPerDonwload.Reset();
+
+                    //Download the folder file
+                    string tempFolderPath1 = CommonUnitity.GetFolderUrl(file);
+                    if (!string.IsNullOrEmpty(tempFolderPath1))
+                    {
+                        tempFolderPath = Path.Combine(CommonUnitity.SystemBinUrl, ConstFile.TEMPFOLDERNAME);
+                        tempFolderPath += tempFolderPath1;
+                    }
+                    else
+                    {
+                        tempFolderPath = Path.Combine(CommonUnitity.SystemBinUrl, ConstFile.TEMPFOLDERNAME);
+                    }
+
+                    clientDownload.DownloadFileAsync(new Uri(file.DownloadUrl), Path.Combine(tempFolderPath, file.FileFullName), file);
+
+                    //Wait for the download complete
+                    evtPerDonwload.WaitOne();
+
+                    clientDownload.Dispose();
+                    clientDownload = null;
+
+                    //Remove the downloaded files
+                    this.downloadFileList.Remove(file);
+                }
+
+            }
+            catch (Exception)
+            {
+                //ShowErrorAndRestartApplication();
+                //throw;
+            }
+
+            //When the files have not downloaded,return.
+            if (downloadFileList.Count > 0)
+            {
+                return;
+            }
+
+            //Test network and deal with errors if there have 
+            DealWithDownloadErrors();
+
+            //Debug.WriteLine("All Downloaded");
+            foreach (DownloadFileInfo file in this.allFileList)
+            {
+                string tempUrlPath = CommonUnitity.GetFolderUrl(file);
+                string oldPath = string.Empty;
+                string newPath = string.Empty;
+                try
+                {
+                    if (!string.IsNullOrEmpty(tempUrlPath))
+                    {
+                        oldPath = Path.Combine(CommonUnitity.SystemBinUrl + tempUrlPath.Substring(1), file.FileName);
+                        newPath = Path.Combine(CommonUnitity.SystemBinUrl + ConstFile.TEMPFOLDERNAME + tempUrlPath, file.FileName);
+                    }
+                    else
+                    {
+                        oldPath = Path.Combine(CommonUnitity.SystemBinUrl, file.FileName);
+                        newPath = Path.Combine(CommonUnitity.SystemBinUrl + ConstFile.TEMPFOLDERNAME, file.FileName);
+                    }
+
+                    //just deal with the problem which the files EndsWith xml can not download
+                    System.IO.FileInfo f = new FileInfo(newPath);
+                    if (!file.Size.ToString().Equals(f.Length.ToString()) && !file.FileName.ToString().EndsWith(".xml"))
+                    {
+                        //ShowErrorAndRestartApplication();
+                    }
+
+
+                    //Added for dealing with the config file download errors
+                    string newfilepath = string.Empty;
+                    if (newPath.Substring(newPath.LastIndexOf(".") + 1).Equals(ConstFile.CONFIGFILEKEY))
+                    {
+                        if (System.IO.File.Exists(newPath))
+                        {
+                            if (newPath.EndsWith("_"))
+                            {
+                                newfilepath = newPath;
+                                newPath = newPath.Substring(0, newPath.Length - 1);
+                                oldPath = oldPath.Substring(0, oldPath.Length - 1);
+                            }
+                            File.Move(newfilepath, newPath);
+                        }
+                    }
+                    //End added
+
+                    if (File.Exists(oldPath))
+                    {
+                        MoveFolderToOld(oldPath, newPath);
+                    }
+                    else
+                    {
+                        //Edit for config_ file
+                        if (!string.IsNullOrEmpty(tempUrlPath))
+                        {
+                            if (!Directory.Exists(CommonUnitity.SystemBinUrl + tempUrlPath.Substring(1)))
+                            {
+                                Directory.CreateDirectory(CommonUnitity.SystemBinUrl + tempUrlPath.Substring(1));
+
+
+                                MoveFolderToOld(oldPath, newPath);
+                            }
+                            else
+                            {
+                                MoveFolderToOld(oldPath, newPath);
+                            }
+                        }
+                        else
+                        {
+                            MoveFolderToOld(oldPath, newPath);
+                        }
+
+                    }
+                }
+                catch (Exception exp)
+                {
+                    _ = exp;
+                    //log the error message,you can use the application's log code
+                }
+
+            }
+
+            //After dealed with all files, clear the data
+            this.allFileList.Clear();
+
+            //if (this.downloadFileList.Count == 0)
+            //    Exit(true);
+            //else
+            //    Exit(false);
+
+            evtDownload.Set();
+        }
+
+        private void DealWithDownloadErrors()
+        {
+            try
+            {
+                //Test Network is OK or not.
+                Config config = Config.LoadConfig(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConstFile.FILENAME));
+                WebClient client = new WebClient();
+                client.DownloadString(config.ServerUrl);
+            }
+            catch (Exception)
+            {
+                //log the error message,you can use the application's log code
+                //ShowErrorAndRestartApplication();
+            }
+        }
+        void MoveFolderToOld(string oldPath, string newPath)
+        {
+            if (File.Exists(oldPath + ".old"))
+                File.Delete(oldPath + ".old");
+
+            if (File.Exists(oldPath))
+                File.Move(oldPath, oldPath + ".old");
+
+
+
+            File.Move(newPath, oldPath);
+            //File.Delete(oldPath + ".old");
+        }
+
+    }
+}
